56af07a4611c21a1162eeccb9b36e75e4c1ee3db
Solar754 <jellyjonkers@gmail.com>
2024-03-10 21:35:31 -0400

    fixed copy paste pathing decision

--- js/map.js
+++ js/map.js
@@ -50,7 +50,7 @@ function mGetTileFlag(x, y) {
     return m.mCurrentMap[x + y * m.mWidthTiles];
 }
 function mCanMoveToTile(x, y) {
-    return (mGetTileFlag(x + 1, y) & (mMOVE_FULL_MASK)) === 0;
+    return (mGetTileFlag(x, y) & (mMOVE_FULL_MASK)) === 0;
 }
 function mCanMoveEast(x, y) {
     return (mGetTileFlag(x + 1, y) & (mMOVE_WEST_MASK | mMOVE_FULL_MASK)) === 0;
